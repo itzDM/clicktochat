5ee463e7a3c33e1d9fe4270d2142d676b7ba3080
itzDm <bitdev20@gamil.com>
2024-03-15 15:09:51 +0530

    new

---
+++ README.md
@@ -0,0 +1,17 @@
+# [ClickToChat](https://itzdm.github.io/clicktochat/)
+
+![clicktochat](https://raw.githubusercontent.com/itzDM/clicktochat/main/images/screenshot.png)
+
+# Click To Chat
+
+## Overview:
+
+Click to chat is WhatsApp's feature allows you to begin a chat with someone without having their phone number saved in your phone's address book. As long as you know this person’s phone number and they have an active WhatsApp account, you can create a link that will allow you to start a chat with them. By clicking the link, a chat with the person automatically opens. Click to chat works on both your phone and WhatsApp Web.
+
+## Features:
+
+1. **Error Handling**: An intuitive error handling mechanism ensures that users are notified of any errors that may occur during the process of creating a link.
+
+2. **Create your own link**: Easily create your own link with just a few clicks.
+
+3. **Chat to App/Web**: You Can Choose to Chat to App or WhatsApp Web .
---
+++ script.js
@@ -0,0 +1,61 @@
+const inputForm = document.querySelector(".form");
+const link = document.querySelector(".link");
+const chatInfo = document.querySelector(".chatInfo");
+const createLinkMob = document.createElement("a");
+const createLinkWeb = document.createElement("a");
+const input = document.querySelector(".input");
+const error = document.querySelector(".error");
+
+const validateMobileNumber = (number) => {
+  const phoneNumberRegex = /^(\+\d{1,3})?\d{10,15}$/;
+  if (!Number(number)) {
+    return { error: "Not A Valid Mobile Number" };
+  } else if (phoneNumberRegex.test(number)) {
+    return { phoneNumber: number };
+  } else {
+    return { error: "Not A Valid Mobile Number" };
+  }
+};
+
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  createLinkMob.remove();
+  createLinkWeb.remove();
+  chatInfo.textContent = "";
+  if (input.value === "") {
+    error.textContent = "Please enter a Mobile Number";
+    const interval = setInterval(() => {
+      error.textContent = "";
+      clearInterval(interval);
+    }, 2000);
+    return;
+  } else {
+    const isNumber = input.value.replace(/\s/g, "");
+    const result = validateMobileNumber(isNumber);
+    if (result.error) {
+      error.textContent = result.error;
+      const interval = setInterval(() => {
+        error.textContent = "";
+        clearInterval(interval);
+      }, 2000);
+      return;
+    }
+    let number = result.phoneNumber;
+    if (number.length === 10) {
+      number = "+91" + number;
+    }
+    chatInfo.textContent = `Chat on WhatsApp with ${number}`;
+    const urlWeb = `https://web.whatsapp.com/send/?phone=${number}&text&type=phone_number&app_absent=0`;
+    const urlMob = `https://api.whatsapp.com/send/?phone=${number}&text&type=phone_number&app_absent=0"`;
+    createLinkMob.href = urlMob;
+    createLinkMob.target = "_blank";
+    createLinkMob.textContent = "Continue to Chat app";
+    createLinkMob.classList.add("btn");
+    link.appendChild(createLinkMob);
+    createLinkWeb.href = urlWeb;
+    createLinkWeb.target = "_blank";
+    createLinkWeb.textContent = "Continue to Chat web";
+    createLinkWeb.classList.add("btn");
+    link.appendChild(createLinkWeb);
+  }
+});
